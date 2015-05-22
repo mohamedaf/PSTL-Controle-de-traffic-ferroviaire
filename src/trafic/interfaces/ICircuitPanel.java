df2dac2b3efad5e6eba5604109ad734d0e101abc
AFFES Mohamed <mohamed.af@hotmail.fr>
2015-05-23 01:24:05 +0200

    ajout rapport-slide (non finaux)

--- src/trafic/interfaces/ICircuitPanel.java
+++ src/trafic/interfaces/ICircuitPanel.java
@@ -7,32 +7,32 @@ import javax.swing.JPanel;
  */
 public interface ICircuitPanel {
 
-	/**
-	 * Indique que le train a avance d'un pas
-	 * 
-	 * @param trainId
-	 *            Id du train qui avance
-	 */
-	public void step(int trainId);
+    /**
+     * Indique que le train a avance d'un pas
+     * 
+     * @param trainId
+     *            Id du train qui avance
+     */
+    public void step(int trainId);
 
-	/**
-	 * @return le composant actuel
-	 */
-	public JPanel getComponent();
+    /**
+     * @return le composant actuel
+     */
+    public JPanel getComponent();
 
-	/**
-	 * Change la couleur d'un feu
-	 * 
-	 * @param lightId
-	 *            Id du feu dont on change la couleur
-	 */
-	void switchLight(int lightId);
+    /**
+     * Change la couleur d'un feu
+     * 
+     * @param lightId
+     *            Id du feu dont on change la couleur
+     */
+    void switchLight(int lightId);
 
-	/**
-	 * Notifie d'un up du capteur
-	 * 
-	 * @param sensorId
-	 *            Capteur active
-	 */
-	public void notifyUp(int sensorId);
+    /**
+     * Notifie d'un up du capteur
+     * 
+     * @param sensorId
+     *            Capteur active
+     */
+    public void notifyUp(int sensorId);
 }
